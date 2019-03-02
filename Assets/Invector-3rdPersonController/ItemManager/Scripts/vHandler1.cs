ae92ac8d4b6d819b2785979fb8d57c7a111014a2
Vishal-iMac <vlxmenblack@gmail.com>
2019-03-02 16:28:19 +0530

    uploaded from iMac
    zz

---
+++ Assets/Invector-3rdPersonController/ItemManager/Scripts/vHandler1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+namespace Invector
+{
+    [System.Serializable]
+    public class vHandler1
+    {
+        public Transform defaultHandler;
+        public List<Transform> customHandlers;
+        public vHandler1()
+        {
+            customHandlers = new List<Transform>();
+        }
+    }
+}
